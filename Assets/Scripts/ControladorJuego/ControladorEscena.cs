bfebe96dd7645baa04741e9797607f843b052a12
Eric <ericmuriel@hotmail.com>
2020-09-11 23:42:11 +0200

    Menu Principal con boton usable hecho

---
+++ Assets/Scripts/ControladorJuego/ControladorEscena.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControladorEscena : MonoBehaviour
+{
+    public void ComenzarPartida()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}

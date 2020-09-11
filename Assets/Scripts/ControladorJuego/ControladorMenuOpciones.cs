121e74f6a144afefd1befeff5cefb88eb84e15b2
Eric <ericmuriel@hotmail.com>
2020-09-11 23:27:04 +0200

    Menu de pausa y opciones hecho en placeholder

---
+++ Assets/Scripts/ControladorJuego/ControladorMenuOpciones.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorMenuOpciones : MonoBehaviour
+{
+    public GameObject menuDeOpciones;
+    public GameObject menuDePausa;
+    [SerializeField] public bool menuOpcionesActivo;
+    ControladorMenuPausa controladorMenuPausa;
+
+    private void Start()
+    {
+        controladorMenuPausa = GetComponent<ControladorMenuPausa>();
+    }
+    public void VolverAlMenuPrincipal()
+    {
+        controladorMenuPausa.estaPausado = true;
+        menuOpcionesActivo = true;
+        menuDeOpciones.SetActive(false);
+        menuDePausa.SetActive(true);
+    }
+}
